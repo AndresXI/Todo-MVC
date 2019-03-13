71f62f46ae68a15e4bebbfa9c3b1be8e8c244b6d
andres alcocer <andresalcocer7@yahoo.com>
2019-03-13 00:17:11 -0400

    updated code to follow MCV pattern

--- js/models/todo.js
+++ js/models/todo.js
@@ -2,5 +2,10 @@ var TodoModel = Backbone.Model.extend({
   defaults: {
     title: '',
     completed: false
+  },
+
+  // Toggle the 'completed' state of this todo item
+  toggle: function () {
+    this.set('completed', !this.get('completed'));
   }
 });
--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -2,35 +2,44 @@
 TodoView = Backbone.View.extend({
   //... is a list tag.
   tagName: 'li',
-
+  // events --> change model ---> update view
   events: {
 
-    'click .toggle': 'checkComplete',
-    'click .destroy': 'clear'
+    'click .toggle': 'toggleCompleted',
+    'click .destroy': 'clear',
+  },
+
+  // The todo view listens for its changes to its model, re-rendering.
+  // There is a one-to-one relationship, so we se a direct reference on the model
+  initialize: function () {
+    // set event listeners to the model to update view
+    this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
   },
 
   // Cache the template function for a single item.
   template: Handlebars.compile($('#item-template').html()),
 
+  // render should only update the view base on the model's changes
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
-
+    // update view on the model boolean value of 'completed'
+    this.$el.toggleClass('completed', this.model.get('completed'));
     return this;
   },
 
-  checkComplete: function () {
-    this.$el.toggleClass('completed');
-
-    if (this.model.get('completed')) {
-      this.model.set('completed', false);
-    } else {
-      this.model.set('completed', true);
-    }
+  /**
+  * The following functions should only be updating the model,
+  * not the view
+  */
+  // Toggle completed state of the model
+  toggleCompleted: function () {
+    this.model.toggle();
   },
 
-
+  // remove item from the view and destroy from collection
   clear: function () {
-    this.remove();
+    this.model.destroy();
   }
 
 });
